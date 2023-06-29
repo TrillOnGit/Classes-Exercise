ca4e11876a78e48cdd31fe8a1980951a63392e7f
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-29 11:54:14 -0400

    Created custom class car and completed exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,14 @@
+namespace Classes;
+
+public class Car
+{
+    public Car()
+    {
+            
+    }
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public int Year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Car Porsche = new Car();
+
+            Porsche.Make = "Volkswagen";
+            Porsche.Model = "2023 Porsche 911";
+            Porsche.Year = 2023;
         }
     }
 }
+
